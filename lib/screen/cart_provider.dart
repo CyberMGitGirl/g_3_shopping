332232e48b72eb6fd4e321cb65294ba9055a6539
Esther <estheticallybawo@gmail.com>
2025-11-05 21:26:52 +0100

    Start Cart Update

--- lib/screen/cart_provider.dart
+++ lib/screen/cart_provider.dart
@@ -2,7 +2,6 @@ import 'dart:collection';
 
 import 'package:flutter/material.dart';
 import 'package:g_3_shopping/screen/item_model.dart';
-import 'package:g_3_shopping/screen/catalog_screen.dart';
 
 class CartProvider extends ChangeNotifier {
   final List<Item> _cartItems = [];
